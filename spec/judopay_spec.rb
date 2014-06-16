1b256cf02baa256d61813e6793fb00c6cd67b29c
bluefuton <chris@bluefuton.com>
2014-06-16 17:34:50 +0100

    Started to write a test for Response class
    We will need to set up WebMock first

--- spec/judopay_spec.rb
+++ spec/judopay_spec.rb
@@ -18,3 +18,26 @@ describe Judopay::Transaction do
     expect(Judopay::Transaction.all).to be_a(Hash)
   end
 end
+
+describe Judopay::Response do
+  {
+    400 => Judopay::BadRequest,
+    404 => Judopay::NotFound,
+    500 => Judopay::InternalServerError,
+    503 => Judopay::ServiceUnavailable
+  }.each do |status, exception|
+    context "when HTTP status is #{status}" do
+
+      # before do
+      #   stub_get('users/self/feed.json').
+      #     to_return(:status => status)
+      # end
+
+      # it "should raise #{exception.name} error" do
+      #   lambda do
+      #     @client.user_media_feed()
+      #   end.should raise_error(exception)
+      # end
+    end
+  end
+end
