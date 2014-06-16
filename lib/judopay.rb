b568b3af692d4cb467b13cafed56b89814838321
bluefuton <chris@bluefuton.com>
2014-06-16 15:25:07 +0100

    Switched to a much more lightweight configuration approach

--- lib/judopay.rb
+++ lib/judopay.rb
@@ -2,8 +2,33 @@ require "judopay/version"
 require File.expand_path('../judopay/api', __FILE__)
 require File.expand_path('../judopay/payment', __FILE__)
 require File.expand_path('../judopay/transaction', __FILE__)
-require File.expand_path('../judopay/configuration', __FILE__)
+require File.expand_path('../judopay/response', __FILE__)
+
 
 module Judopay
-  extend Configuration
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :api_version, 
+                  :api_token,
+                  :api_secret,
+                  :format, 
+                  :endpoint_url,
+                  :user_agent
+
+    def initialize
+      # Set defaults
+      @api_version = '4.0.0'
+      @format = 'json'
+      @endpoint_url = 'https://partnerapi.judopay-sandbox.com'
+      @user_agent = 'Judopay Ruby SDK gem v' + Judopay::VERSION
+    end
+  end
 end
