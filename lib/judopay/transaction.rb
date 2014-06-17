533f14fbd17205a395e54dc4a1a8e71ce0c477e2
bluefuton <chris@bluefuton.com>
2014-06-17 15:44:51 +0100

    Implemented get single transaction

--- lib/judopay/transaction.rb
+++ lib/judopay/transaction.rb
@@ -4,5 +4,10 @@ module Judopay
       api = Judopay::API.new
       api.get('transactions')
     end
+
+    def self.find(receipt_id)
+      api = Judopay::API.new
+      api.get('transactions/' + receipt_id.to_i.to_s)
+    end
   end
 end
