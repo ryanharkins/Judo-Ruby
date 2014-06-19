e63197abc5a838012ffa3f2702ee8c5e685b9f38
bluefuton <chris@bluefuton.com>
2014-06-19 16:15:50 +0100

    Added rspec test for transaction.save

--- spec/judopay/transaction_spec.rb
+++ spec/judopay/transaction_spec.rb
@@ -21,4 +21,25 @@ describe Judopay::Transaction do
     expect(transaction).to be_a(Hash)
     expect(transaction.receipt_id).to eq(receipt_id)                   
   end
+
+  it "should save a new transaction given valid card details" do
+    stub_post('/transactions/payments').
+      to_return(:status => 200,
+                :body => lambda { |request| fixture("transactions/save.json") })
+
+    transaction = Judopay::Transaction.new(
+      :your_consumer_reference => '123',
+      :your_payment_reference => '456',
+      :judo_id => '123-456',
+      :amount => 1.01,
+      :card_number => '4976000000003436',
+      :expiry_date => '12/15',
+      :cv2 => '452'
+    )
+    
+    response = transaction.save
+
+    expect(response).to be_a(Hash)
+    expect(response.result).to eq("Success")
+  end
 end
