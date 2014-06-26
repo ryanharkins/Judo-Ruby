57f9d00a1f2100a601a0983fc9fdee3e53686d41
bluefuton <chris@bluefuton.com>
2014-06-26 11:41:06 +0100

    Added Collection.create

--- lib/judopay/models/collection.rb
+++ lib/judopay/models/collection.rb
@@ -3,6 +3,14 @@ require_relative '../model'
 module Judopay
   class Collection < Model
     @resource_path = 'transactions/collections'
-    @valid_api_methods = [:all]
+    @valid_api_methods = [:all, :create]
+
+    attribute :receipt_id, Integer
+    attribute :amount, Float
+    attribute :your_payment_reference, String
+
+    validates_presence_of :receipt_id,
+                          :amount,
+                          :your_payment_reference
   end  
 end
