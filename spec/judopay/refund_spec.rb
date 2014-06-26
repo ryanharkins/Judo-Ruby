faa3016712dec050e88b8aaf41eb1bd2c82fa07d
bluefuton <chris@bluefuton.com>
2014-06-26 11:51:07 +0100

    Added Refund.create

--- spec/judopay/refund_spec.rb
+++ spec/judopay/refund_spec.rb
@@ -11,4 +11,21 @@ describe Judopay::Refund do
     expect(transactions).to be_a(Hash)
     expect(transactions.results[0].amount).to equal(1.01)
   end
+
+  it "should create a new refund given a valid payment reference" do
+    stub_post('/transactions/refunds').
+      to_return(:status => 200,
+                :body => lambda { |request| fixture("card_payments/create.json") })
+
+    payment = Judopay::Refund.new(
+      :receipt_id => '1234',
+      :amount => 1.01,
+      :your_payment_reference => 'payment12412312'
+    )
+    
+    response = payment.create
+
+    expect(response).to be_a(Hash)
+    expect(response.result).to eq("Success")
+  end    
 end
