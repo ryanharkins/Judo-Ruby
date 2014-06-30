46927941aaea7cd4148e4750c3f1f81f11a6f28c
bluefuton <chris@bluefuton.com>
2014-06-30 17:23:40 +0100

    String util method to convert camelcase to underscore

--- lib/patches/string.rb
+++ lib/patches/string.rb
@@ -14,5 +14,18 @@ class String
 
   def uncapitalize 
     self[0, 1].downcase + self[1..-1]
+  end
+
+  # converts a camel_cased string to a underscore string
+  # subs spaces with underscores, strips whitespace
+  def underscore
+    self.to_s.strip.
+      gsub(' ', '_').
+      gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      squeeze("_").
+      downcase
   end  
 end
