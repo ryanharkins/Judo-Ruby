a95655bc8fb0febe69e24b4fe8a3c8895ff4e461
bluefuton <chris@bluefuton.com>
2014-07-03 16:53:12 +0100

    Fixed indentation in the WebPayments::Payment

--- lib/judopay/models/web_payments/payment.rb
+++ lib/judopay/models/web_payments/payment.rb
@@ -6,23 +6,23 @@ module Judopay
       @resource_path = 'webpayments/payments'
       @valid_api_methods = [:find, :create]
 
-    attribute :judo_id, String
-    attribute :amount, Float
-    attribute :partner_service_fee, Float
-    attribute :your_consumer_reference, String
-    attribute :your_payment_reference, String
-    attribute :your_payment_meta_data, Hash
-    attribute :client_ip_address, String
-    attribute :client_user_agent, String
+      attribute :judo_id, String
+      attribute :amount, Float
+      attribute :partner_service_fee, Float
+      attribute :your_consumer_reference, String
+      attribute :your_payment_reference, String
+      attribute :your_payment_meta_data, Hash
+      attribute :client_ip_address, String
+      attribute :client_user_agent, String
 
-    validates_presence_of :judo_id,
-                          :your_consumer_reference,
-                          :your_payment_reference,
-                          :amount
+      validates_presence_of :judo_id,
+                            :your_consumer_reference,
+                            :your_payment_reference,
+                            :amount
 
       # Find a specific web payment given a valid payment reference
       #
-      # @param [String] Payment reference
+      # @param reference [String] Payment reference
       # @return [Judopay::Mash] Mash of the API response   
       def self.find(reference)
         api = Judopay::API.new
