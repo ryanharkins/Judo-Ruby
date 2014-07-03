4fad204c355fc29437d364a1c5032b106a742848
bluefuton <chris@bluefuton.com>
2014-07-03 16:33:07 +0100

    Documented model.rb for yard

--- lib/judopay/model.rb
+++ lib/judopay/model.rb
@@ -17,7 +17,11 @@ module Judopay
       attr_accessor :resource_path, :valid_api_methods
     end
 
-    def self.all(options = {})
+    # List all records
+    #
+    # @param paging_options [Hash] Paging options (sort, offset and page_size)
+    # @return [Judopay::Mash] Mash of the API response   
+    def self.all(paging_options = {})
       check_api_method_is_supported(__method__)
       api = Judopay::API.new
       valid_options = self.valid_options(options).camel_case_keys!
@@ -25,12 +29,19 @@ module Judopay
       api.get(uri)
     end
 
+    # Retrieve a specific record
+    #
+    # @param receipt_id [Integer] Paging options (sort, offset and page_size)
+    # @return [Judopay::Mash] Mash of the API response   
     def self.find(receipt_id)
       check_api_method_is_supported(__method__)
       api = Judopay::API.new
       api.get(resource_path + receipt_id.to_i.to_s)
     end
 
+    # Create a new record
+    #
+    # @return [Judopay::Mash] Mash of the API response
     def create
       check_api_method_is_supported(__method__)
       check_judo_id
@@ -39,20 +50,28 @@ module Judopay
       api.post(resource_path, self)
     end
 
+    # Retrieve the current API resource path (e.g. /transactions/payments)
+    #
+    # @return [String] Resource path
     def resource_path
       self.class.resource_path
     end
 
+    # Retrieve an array of valid API methods for the current model
+    # e.g [:find, :create]
+    #
+    # @return [Array<Symbol>] Valid API methods
     def valid_api_methods
       self.class.valid_api_methods
     end
 
+    # Verify if an API method is supported for the current model
     def check_api_method_is_supported(method)
       self.class.check_api_method_is_supported(method)
     end
 
+    # Use judo_id from configuration if it hasn't been explicitly set
     def check_judo_id
-      # Use judo_id from config if it hasn't been explicitly set
       if self.respond_to?('judo_id') && judo_id.nil?
         self.judo_id = Judopay.configuration.judo_id
       end
@@ -62,17 +81,23 @@ module Judopay
 
     # Has the pre-validation found any problems?
     # We check the basics have been completed to avoid round trip to API
+    #
+    # @return nil
+    # @raise [ValidationError] if there are validation errors on the model   
     def check_validation
       fail Judopay::ValidationError, errors unless valid?
     end
 
+    # Check if the specified API method is supported by the current model
+    #
+    # @raise [ValidationError] if the API method is not supported
     def self.check_api_method_is_supported(method)
       if valid_api_methods.nil? || !valid_api_methods.include?(method.to_sym)
         fail Judopay::Error, 'API method not supported'
       end
     end
 
-    # Take an options hash and filter all but the valid keys
+    # Take an paging options hash and filter all but the valid keys
     def self.valid_options(options)
       valid_options = {}
       options.each do |key, value|
--- lib/judopay/models/web_payments/payment.rb
+++ lib/judopay/models/web_payments/payment.rb
@@ -20,8 +20,10 @@ module Judopay
                           :your_payment_reference,
                           :amount
 
-      # Web payments uses a different endpoint for finding transactions (/webpayments)
-      # Reference is a string (standard payments use an integer)
+      # Find a specific web payment given a valid payment reference
+      #
+      # @param [String] Payment reference
+      # @return [Judopay::Mash] Mash of the API response   
       def self.find(reference)
         api = Judopay::API.new
         api.get('webpayments/' + reference.to_s)
