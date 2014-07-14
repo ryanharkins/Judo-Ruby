b31dd969537ee566a2c3f9f429e189ab7bd38e88
bluefuton <chris@bluefuton.com>
2014-07-14 11:08:50 +0100

    Rubocop quote cleanup

--- spec/judopay/core_ext/string_spec.rb
+++ spec/judopay/core_ext/string_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 require_relative '../../../lib/judopay/core_ext/string'
 
 describe String do
-  it "should convert an underscored string to camel case" do
-    expect("judo_pay".camel_case).to eq('judoPay')
+  it 'should convert an underscored string to camel case' do
+    expect('judo_pay'.camel_case).to eq('judoPay')
   end
 
-  it "should convert an camel case string to underscored format" do
-    expect("JudoPay".underscore).to eq('judo_pay')
+  it 'should convert an camel case string to underscored format' do
+    expect('JudoPay'.underscore).to eq('judo_pay')
   end
 
-  it "should uncapitalize a word" do
-    expect("JudoPay".uncapitalize).to eq('judoPay')
+  it 'should uncapitalize a word' do
+    expect('JudoPay'.uncapitalize).to eq('judoPay')
   end
 end
