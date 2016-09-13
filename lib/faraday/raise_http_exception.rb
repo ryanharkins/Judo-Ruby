747cc864e10a2abf840a373d643cd1abf999b3bd
Oleg Fetisov <ofetisov@corevalue.net>
2016-09-13 17:29:09 +0300

    Error model change

--- lib/faraday/raise_http_exception.rb
+++ lib/faraday/raise_http_exception.rb
@@ -7,24 +7,7 @@ module FaradayMiddleware
   class RaiseHttpException < Faraday::Middleware
     def call(env)
       @app.call(env).on_complete do |response|
-        case response.status.to_i
-        when 400
-          raise Judopay::BadRequest, response
-        when 401, 403
-          raise Judopay::NotAuthorized, response
-        when 404
-          raise Judopay::NotFound, response
-        when 409
-          raise Judopay::Conflict, response
-        when 500
-          raise Judopay::InternalServerError, response
-        when 502
-          raise Judopay::BadGateway, response
-        when 503
-          raise Judopay::ServiceUnavailable, response
-        when 504
-          raise Judopay::GatewayTimeout, response
-        end
+        raise Judopay::APIError.factory(response) unless response.status.to_i == 200
       end
     end
 
--- spec/judopay/error_spec.rb
+++ spec/judopay/error_spec.rb
@@ -1,23 +1,40 @@
 require 'spec_helper'
 require_relative '../../lib/judopay/error'
+require 'active_model'
 
-describe Judopay::Error do
-  it "returns a 'message' equal to the class name if the message is not set, because 'message' should not be nil" do
-    e = Judopay::Error.new
-    expect(e.message).to eq('Judopay::Error')
+describe Judopay::FieldError do
+  it 'returns fields that was set in message' do
+    e = Judopay::FieldError.new('msg', 123, 'field', 'detail')
+    expect(e.to_s).to eq('Field "field" (code 123): msg')
   end
+end
 
-  it "returns the 'message' that was set" do
+describe Judopay::ValidationError do
+  it 'returns fields that was set' do
+    errors = ActiveModel::Errors.new(nil)
+    errors['some_field'] = 'some error'
+    e = Judopay::ValidationError.new('Error', errors)
+    expect(e.to_s).to eq("Error\nField errors:\nsome_field: some error")
+  end
+end
+
+describe Judopay::APIError do
+  it 'returns the fields that was set' do
     message = 'An explicitly set message'
-    e = Judopay::Error.new(message)
+
+    e = Judopay::APIError.new(message, 1, 12, 23, [])
     expect(e.message).to eq(message)
+    expect(e.error_code).to eq(1)
+    expect(e.status_code).to eq(12)
+    expect(e.category).to eq(23)
+    expect(e.field_errors).to eq([])
   end
 
-  it 'contains exceptions in Judopay' do
-    expect(Judopay::BadRequest.new).to be_a_kind_of(Judopay::APIError)
+  it 'return class name when there is no message and field errors' do
+    expect(Judopay::APIError.new(nil).message).to eq('Judopay::APIError')
   end
 
-  it 'makes error information available on the exception object for API errors' do
+  it 'creates valid APIError from response using factory method' do
     stub_post('/transactions/payments').
       to_return(:status => 400,
                 :body => lambda { |_request| fixture('card_payments/create_bad_request.json') },
@@ -27,11 +44,16 @@ describe Judopay::Error do
 
     begin
       payment.create
-    rescue Judopay::BadRequest => e
-      expect(e.http_status).to eq(400)
-      expect(e.model_errors).to be_a_kind_of(Hash)
-      expect(e.error_type).to eq(9)
-      expect(e.message).to eq('Please check the card token.')
+    rescue Judopay::APIError => e
+      expect(e.status_code).to eq(400)
+      expect(e.field_errors).to be_a_kind_of(Array)
+      expect(e.field_errors[0]).to be_a_kind_of(Judopay::FieldError)
+      expect(e.field_errors[0].code).to eq(46)
+      expect(e.error_code).to eq(1)
+      expect(e.category).to eq(2)
+      expect(e.message).to eq("Sorry, we're unable to process your request. Please check your details and try again.\n\
+Fields errors:\nField \"ExpiryDate\" (code 46): Sorry, but the expiry date entered is in the past. \
+Please check your details and try again.")
     end
   end
 
