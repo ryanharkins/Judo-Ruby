2b5ca66fa2e14e9a039ca0f6c44d3e0a95c38a0d
Edwin Wills <edwin@rentify.com>
2016-10-18 14:01:50 +0100

    Use the existing monkeypatched underscore method on String

--- lib/judopay/mash.rb
+++ lib/judopay/mash.rb
@@ -6,15 +6,7 @@ module Judopay
     protected
 
     def convert_key(key)
-      camelcase_to_underscore(key.to_s)
-    end
-
-    def camelcase_to_underscore(string)
-      string.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-", "_").
-      downcase
+      key.to_s.underscore
     end
   end
 end
